11be478b0b5b851b891cb6fe510b6c443a36f42f
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-09-14 16:35:33 +0400

    Change response method name

--- rest/Controller.php
+++ rest/Controller.php
@@ -16,7 +16,7 @@ class Controller extends \yii\rest\Controller
      * @param $statusCode
      * @return array
      */
-    public static function validationError($message, $statusCode = 422)
+    public static function response($message, $statusCode)
     {
         Yii::$app->response->setStatusCode($statusCode);
         return $message;
