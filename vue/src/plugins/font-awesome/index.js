6712cfc38ae1cc27efa0e44776d1375cd7998831
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-09-16 19:25:27 +0400

    Add some css and icons

--- vue/src/plugins/font-awesome/index.js
+++ vue/src/plugins/font-awesome/index.js
@@ -1,9 +1,28 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {faSquareFull, faCompress, faTasks} from '@fortawesome/free-solid-svg-icons'
-import {faEnvelope, faBell} from '@fortawesome/free-regular-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+  faSquareFull,
+  faCompress,
+  faTasks,
+  faFilter,
+  faPlusCircle,
+  faSearch,
+  faUndo,
+  faPencilAlt,
+  faEllipsisV,
+  faTimes
+} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope, faBell, faTrashAlt} from '@fortawesome/free-regular-svg-icons'
 
 library.add(faSquareFull);
 library.add(faCompress);
 library.add(faEnvelope);
 library.add(faBell);
 library.add(faTasks);
+library.add(faFilter);
+library.add(faPlusCircle);
+library.add(faSearch);
+library.add(faUndo);
+library.add(faPencilAlt);
+library.add(faTrashAlt);
+library.add(faEllipsisV);
+library.add(faTimes);
