77d6c7cbe0b777c520dea02987755129f06ae368
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-09-18 13:29:10 +0400

    Get password reset link from helper

--- helpers/MailHelper.php
+++ helpers/MailHelper.php
@@ -35,7 +35,11 @@ class MailHelper
      */
     public static function resetPassword($user)
     {
-        $message = Yii::$app->mailer->compose('reset_password', ['user' => $user])
+        $message = Yii::$app->mailer->compose('reset_password',
+            [
+                'user' => $user,
+                'link' => env('PORTAL_HOST') . "/auth/password-reset/$user->password_reset_token"
+            ])
             ->setSubject(Yii::t('app', 'Your new password'))
             ->setTo($user->email);
 
