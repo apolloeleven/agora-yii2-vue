662d5b9eab14c8dbda253011bfb1a4fced200e09
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-09-22 14:15:05 +0400

    Fix tree generation logic

---
+++ vue/src/store/setup/getters.js
@@ -0,0 +1,17 @@
+import _ from "lodash";
+
+const getTree = (departments, parentId = null) => {
+  const nodes = [];
+  for (const item of departments) {
+    if (item.parent_id === parentId) {
+      item.children = getTree(departments, item.id);
+      nodes.push(item);
+    }
+  }
+
+  return nodes
+}
+
+export function departmentsTree(state) {
+  return getTree(state.departments.data, null)
+}
