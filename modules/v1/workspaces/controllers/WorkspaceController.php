d39b18b4519c5d1e33539250ece5ff2d43c75208
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-09-24 18:58:32 +0400

    Create workspace controller

---
+++ modules/v1/workspaces/controllers/WorkspaceController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\modules\v1\workspaces\controllers;
+
+
+use app\rest\ActiveController;
+
+/**
+ * Class WorkspaceController
+ *
+ */
+class WorkspaceController extends ActiveController
+{
+    /**
+     * @return array
+     */
+    public function actions()
+    {
+        return parent::actions();
+    }
+}
