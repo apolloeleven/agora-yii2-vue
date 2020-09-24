9785f0727045a8834bfc6ebbeb78ac30ecbc99e2
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-09-24 18:56:02 +0400

    Create workspace model and query

---
+++ modules/v1/workspaces/models/Workspace.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace app\modules\v1\workspaces\models;
+
+use app\models\User;
+use app\modules\v1\workspaces\models\query\WorkspaceQuery;
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "{{%workspaces}}".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $abbreviation
+ * @property string|null $description
+ * @property string|null $image_path
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $updated_by
+ *
+ * @property User $createdBy
+ * @property User $updatedBy
+ */
+class Workspace extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%workspaces}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            TimestampBehavior::class,
+            BlameableBehavior::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['description'], 'string'],
+            [['created_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['abbreviation'], 'string', 'max' => 55],
+            [['image_path'], 'string', 'max' => 1024],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['updated_by' => 'id']],
+            [['image'], 'file', 'skipOnEmpty' => true, 'extensions' => 'png, jpeg, svg, gif, jpg']
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'abbreviation' => Yii::t('app', 'Abbreviation'),
+            'description' => Yii::t('app', 'Description'),
+            'image_path' => Yii::t('app', 'Image Path'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'updated_by' => Yii::t('app', 'Updated By'),
+        ];
+    }
+
+    /**
+     * @return WorkspaceQuery|ActiveQuery
+     */
+    public static function find()
+    {
+        return new WorkspaceQuery(get_called_class());
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'updated_by']);
+    }
+}
---
+++ modules/v1/workspaces/models/query/WorkspaceQuery.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace app\modules\v1\workspaces\models\query;
+
+use app\modules\v1\workspaces\models\Workspace;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * Class WorkspaceQuery
+ *
+ * @package app\modules\v1\workspaces\models\query
+ */
+class WorkspaceQuery extends ActiveQuery
+{
+    /**
+     * @param null $db
+     * @return Workspace[]
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @param null $db
+     * @return Workspace|array|ActiveRecord
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+
+    /**
+     * @param $id
+     * @return WorkspaceQuery
+     */
+    public function byId($id)
+    {
+        return $this->andWhere([Workspace::tableName() . '.id' => $id]);
+    }
+}
