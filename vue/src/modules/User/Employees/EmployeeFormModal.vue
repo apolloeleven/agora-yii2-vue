bc0f306e51de3051899ba3c584f4128de78d42b1
Nika Gelashvili <nika.gelash@gmail.com>
2020-09-25 19:07:52 +0400

    Improve employee form modal visually

--- vue/src/modules/User/Employees/EmployeeFormModal.vue
+++ vue/src/modules/User/Employees/EmployeeFormModal.vue
@@ -1,14 +1,24 @@
 <template>
   <ValidationObserver ref="form" v-slot="{ handleSubmit, invalid ,reset}">
     <b-modal
-        :visible="showModal" id="user-form" ref="modal" :title='$t(`Edit employee`)' @hidden="onHideModal"
+        :visible="showModal" id="user-form" ref="modal" :title='$t(`Edit employee`)' @hidden="onHideModal" size="lg"
         @ok.prevent="handleSubmit(onSubmit)" :ok-disabled="loading" :ok-title="$t('Submit')" scrollable>
       <content-spinner :show="loading" :text="$t('Please wait...')" :fullscreen="true" class="h-100"/>
       <b-form @submit.prevent="handleSubmit(onSubmit)" novalidate>
-        <input-widget :model="model" attribute="first_name"/>
-        <input-widget :model="model" attribute="last_name"/>
-        <input-widget :model="model" attribute="email"/>
-        <b-card header-tag="header" footer-tag="footer" class="form-cards mb-3" body-class="p-2">
+        <div class="row">
+          <div class="col-md-12">
+            <input-widget :model="model" attribute="email"/>
+          </div>
+          <div class="col-md-6">
+            <input-widget :model="model" attribute="first_name"/>
+          </div>
+          <div class="col-md-6">
+            <input-widget :model="model" attribute="last_name"/>
+          </div>
+        </div>
+
+
+        <b-card header-tag="header" footer-tag="footer" class="form-cards mb-3" body-class="pb-0">
           <template v-slot:header>
             <div class="d-flex align-items-center">
               <h5 class="mb-0">{{ $t('Roles') }}</h5>
@@ -18,26 +28,32 @@
               </b-button>
             </div>
           </template>
-          <div v-for="(userRoleModel, index) in model.roles" :key="index">
-            <div class="row">
-              <div class="col col-1">
-                <b-form-group>
-                  <b-button size="sm" v-b-tooltip :title="$t('Remove role')" pill v-on:click="removeRole(index)"
-                            variant="outline-danger">
-                    <i class="fa fa-times"></i>
-                  </b-button>
-                </b-form-group>
-              </div>
-              <div class="col col-5">
-                <b-form-group>
-                  <b-form-select v-model="userRoleModel.name" :options="dropdownData.userRoles"/>
-                </b-form-group>
+          <div class="row">
+            <div class="col col-12">
+              <div class="mb-3 " v-for="(userRoleModel, index) in model.roles" :key="index">
+                <div class="row">
+                  <div class="col-sm-1 col-1 d-flex align-items-center">
+                    <b-button v-b-tooltip :title="$t('Remove role')" pill v-on:click="removeRole(index)"
+                              variant="outline-danger" size="sm">
+                      <i class="fa fa-times"></i>
+                    </b-button>
+                  </div>
+                  <div class="col-11">
+                    <div class="row">
+                      <div class="col-sm-12 col-md-6">
+                        <b-form-group class="mb-0">
+                          <b-form-select v-model="userRoleModel.name" :options="dropdownData.userRoles"/>
+                        </b-form-group>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </b-card>
 
-        <b-card header-tag="header" footer-tag="footer" class="form-cards">
+        <b-card header-tag="header" footer-tag="footer" class="form-cards" body-class="pb-0">
           <template v-slot:header>
             <div class="d-flex align-items-center">
               <h5 class="mb-0">{{ $t('Positions') }}</h5>
@@ -51,19 +67,16 @@
             <div class="col col-12">
               <div class="mb-3 " v-for="(userDepartmentModel, index) in model.userDepartments" :key="index">
                 <div class="row">
-                  <div class="col-sm-1 col-1">
-                    <b-form-group>
-                      <label class="d-block">&nbsp;</label>
-                      <b-button v-b-tooltip :title="$t('Remove position')" pill v-on:click="removeUserDepartment(index)"
-                                variant="outline-danger">
-                        <i class="fa fa-times"></i>
-                      </b-button>
-                    </b-form-group>
+                  <div class="col-sm-1 col-1 d-flex align-items-center">
+                    <b-button v-b-tooltip :title="$t('Remove position')" pill v-on:click="removeUserDepartment(index)"
+                              variant="outline-danger" size="sm">
+                      <i class="fa fa-times"></i>
+                    </b-button>
                   </div>
                   <div class="col-11">
                     <div class="row">
                       <div class="col-sm-12 col-md-4">
-                        <b-form-group :label="$t('Country')">
+                        <b-form-group class="mb-0">
                           <b-form-select v-model="userDepartmentModel.country_id"
                                          value-field="id"
                                          text-field="name"
@@ -71,7 +84,7 @@
                         </b-form-group>
                       </div>
                       <div class="col-sm-12 col-md-4">
-                        <b-form-group :label="$t('Department')">
+                        <b-form-group class="mb-0">
                           <b-form-select v-model="userDepartmentModel.department_id"
                                          value-field="id"
                                          text-field="name"
@@ -79,7 +92,7 @@
                         </b-form-group>
                       </div>
                       <div class="col-sm-12 col-md-4">
-                        <b-form-group :label="$t('Position')">
+                        <b-form-group class="mb-0">
                           <b-form-input v-model="userDepartmentModel.position" list="job-title-list"/>
                         </b-form-group>
                       </div>
@@ -90,6 +103,7 @@
             </div>
           </div>
         </b-card>
+
       </b-form>
     </b-modal>
   </ValidationObserver>
