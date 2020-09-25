5934e1450dfe5892ae5255caa743f111d148c120
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-09-25 17:46:39 +0400

    Update user_profile migration. Insert user for id = 1

--- migrations/m200922_072549_create_user_profiles_table.php
+++ migrations/m200922_072549_create_user_profiles_table.php
@@ -36,6 +36,58 @@ class m200922_072549_create_user_profiles_table extends Migration
             'id',
             'CASCADE'
         );
+
+        // creates index for column `created_by`
+        $this->createIndex(
+            '{{%idx-user_profiles-created_by}}',
+            '{{%user_profiles}}',
+            'created_by'
+        );
+
+        // add foreign key for table `{{%users}}`
+        $this->addForeignKey(
+            '{{%fk-user_profiles-created_by}}',
+            '{{%user_profiles}}',
+            'created_by',
+            '{{%users}}',
+            'id',
+            'CASCADE'
+        );
+
+        // creates index for column `updated_by`
+        $this->createIndex(
+            '{{%idx-user_profiles-updated_by}}',
+            '{{%user_profiles}}',
+            'updated_by'
+        );
+
+        // add foreign key for table `{{%users}}`
+        $this->addForeignKey(
+            '{{%fk-user_profiles-updated_by}}',
+            '{{%user_profiles}}',
+            'updated_by',
+            '{{%users}}',
+            'id',
+            'CASCADE'
+        );
+
+        // Insert user for id 1 for user profile
+
+        $this->insert('{{%user_profiles}}', [
+            'user_id' => 1,
+            'first_name' => 'Apollo',
+            'last_name' => '11',
+            'mobile' => null,
+            'phone' => null,
+            'birthday' => null,
+            'about_me' => null,
+            'hobbies' => null,
+            'image_path' => null,
+            'created_at' => time(),
+            'updated_at' => time(),
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
     }
 
     /**
@@ -43,6 +95,31 @@ class m200922_072549_create_user_profiles_table extends Migration
      */
     public function safeDown()
     {
+
+        // drops foreign key for table `{{%users}}`
+        $this->dropForeignKey(
+            '{{%fk-user_profiles-created_by}}',
+            '{{%user_profiles}}'
+        );
+
+        // drops index for column `created_by`
+        $this->dropIndex(
+            '{{%idx-user_profiles-created_by}}',
+            '{{%user_profiles}}'
+        );
+
+        // drops foreign key for table `{{%users}}`
+        $this->dropForeignKey(
+            '{{%fk-user_profiles-updated_by}}',
+            '{{%user_profiles}}'
+        );
+
+        // drops index for column `updated_by`
+        $this->dropIndex(
+            '{{%idx-user_profiles-updated_by}}',
+            '{{%user_profiles}}'
+        );
+
         $this->dropForeignKey(
             '{{%fk-user_profile-user_id}}',
             '{{%user_profiles}}'
