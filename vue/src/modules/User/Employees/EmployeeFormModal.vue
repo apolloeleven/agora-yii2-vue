63d346b9f54adefc16fce1da729bae8414388b97
Nika Gelashvili <nika.gelash@gmail.com>
2020-09-25 13:08:49 +0400

    Edit card styles remove labels form each role and permission added make labels for cards move buttons to right, minimize role remove button. Remove vue-multiselect and using b-form-select for departments. Use dropdown for countries instead of input with options. Implement model usage for roles and positions.

--- vue/src/modules/User/Employees/EmployeeFormModal.vue
+++ vue/src/modules/User/Employees/EmployeeFormModal.vue
@@ -8,29 +8,29 @@
         <input-widget :model="model" attribute="firstName"/>
         <input-widget :model="model" attribute="lastName"/>
         <input-widget :model="model" attribute="email"/>
-        <b-card header-tag="header" footer-tag="footer" class="form-cards">
+        <b-card header-tag="header" footer-tag="footer" class="form-cards mb-3" body-class="p-2">
           <template v-slot:header>
-            <b-form-group>
-              <b-button size="sm" type="button" v-on:click="addNewRole" variant="success">
+            <div class="d-flex align-items-center">
+              <h5 class="mb-0">{{ $t('Roles') }}</h5>
+              <b-button size="sm" type="button" v-on:click="addNewRole" variant="success" class="ml-auto">
                 <i class="fa fa-plus-circle "></i>
-                {{ $t('Add Role') }}
+                {{ $t('Add New') }}
               </b-button>
-            </b-form-group>
+            </div>
           </template>
-          <div v-for="(uw, index) in userRole" :key="index">
+          <div v-for="(userRoleModel, index) in model.roles" :key="index">
             <div class="row">
               <div class="col col-1">
                 <b-form-group>
-                  <label class="d-block">&nbsp;</label>
-                  <b-button v-b-tooltip :title="$t('Remove role')" pill v-on:click="removeRole(index)"
+                  <b-button size="sm" v-b-tooltip :title="$t('Remove role')" pill v-on:click="removeRole(index)"
                             variant="outline-danger">
                     <i class="fa fa-times"></i>
                   </b-button>
                 </b-form-group>
               </div>
               <div class="col col-5">
-                <b-form-group :label="$t('Role')">
-                  <b-form-select v-model="uw.role" :options="dropdownData.userRoles"/>
+                <b-form-group>
+                  <b-form-select v-model="userRoleModel.name" :options="dropdownData.userRoles"/>
                 </b-form-group>
               </div>
             </div>
@@ -39,16 +39,17 @@
 
         <b-card header-tag="header" footer-tag="footer" class="form-cards">
           <template v-slot:header>
-            <b-form-group>
-              <b-button size="sm" type="button" v-on:click="addNewPosition" variant="success">
+            <div class="d-flex align-items-center">
+              <h5 class="mb-0">{{ $t('Positions') }}</h5>
+              <b-button size="sm" type="button" v-on:click="addNewPosition" variant="success" class="ml-auto">
                 <i class="fa fa-plus-circle "></i>
-                {{ $t('Add Position') }}
+                {{ $t('Add New') }}
               </b-button>
-            </b-form-group>
+            </div>
           </template>
           <div class="row">
             <div class="col col-12">
-              <div class="mb-3 " v-for="(dp, index) in userDepartment" :key="index">
+              <div class="mb-3 " v-for="(positionModel, index) in model.positions" :key="index">
                 <div class="row">
                   <div class="col-sm-1 col-1">
                     <b-form-group>
@@ -63,35 +64,25 @@
                     <div class="row">
                       <div class="col-sm-12 col-md-4">
                         <b-form-group :label="$t('Position')">
-                          <b-form-input v-model="dp.position" list="job-title-list"/>
+                          <b-form-input v-model="positionModel.name" list="job-title-list"/>
                           <b-form-datalist id="job-title-list" :options="dropdownData.userPositions"/>
                         </b-form-group>
                       </div>
                       <div class="col-sm-12 col-md-4">
                         <b-form-group :label="$t('Country')">
-                          <b-form-input v-model="dp.country" list="country-list"/>
-                          <b-form-datalist id="country-list" :options="dropdownData.countries"/>
+                          <b-form-select v-model="positionModel.country_id"
+                                         value-field="id"
+                                         text-field="name"
+                                         :options="dropdownData.countries"/>
                         </b-form-group>
                       </div>
                       <div class="col-sm-12 col-md-4">
-                        <b-form-group :label="$t('Departments')" id="department-form">
-                          <multiselect id="department" v-model="dp.department"
-                                       :tag-placeholder="$t('Add department')"
-                                       :placeholder="$t('Search or add a department')"
-                                       :options="filterOptions(dp.country)" :multiple="true"
-                                       :taggable="true" aria-labelledby="feedback-department"
-                                       :selectLabel="$t('Press enter to select')"
-                                       :deselectLabel="$t('Press enter to remove')"
-                                       :selectedLabel="$t('Selected')" track-by="id" label="name"
-                                       @tag="addDepartment(dp, $event)">
-                            <span slot="noOptions">{{ $t('List is empty.') }}</span>
-                            <template slot="tag" slot-scope="{ option, remove }">
-                                <span class="multiselect__tag">
-                                  <span>{{ $t(option.name) }}</span>
-                                  <span class="multiselect__tag-icon" @click="remove(option)"></span>
-                                </span>
-                            </template>
-                          </multiselect>
+                        <b-form-group :label="$t('Country')">
+                          <b-form-select v-model="positionModel.departments"
+                                         multiple
+                                         value-field="id"
+                                         text-field="name"
+                                         :options="getDepartments(positionModel)"/>
                         </b-form-group>
                       </div>
                     </div>
@@ -111,21 +102,20 @@
 import {createNamespacedHelpers} from "vuex";
 import ContentSpinner from "../../../core/components/ContentSpinner";
 import InputWidget from "../../../core/components/input-widget/InputWidget";
-import EmployeeListFormModel from "./EmployeeListFormModel";
-import Multiselect from "vue-multiselect";
+import EmployeeModel from "./EmployeeModel.js";
 import Vue from "vue"
+import PositionModel from "@/modules/User/Employees/PositionModel";
+import RoleModel from "@/modules/User/Employees/RoleModel";
 
 const {mapState, mapActions} = createNamespacedHelpers('user/employees');
 
 export default {
-  name: "EmployeeListForm",
-  components: {ContentSpinner, InputWidget, Multiselect},
+  name: "EmployeeFormModal",
+  components: {ContentSpinner, InputWidget},
   data() {
     return {
       loading: false,
-      model: new EmployeeListFormModel(),
-      userRole: [],
-      userDepartment: []
+      model: new EmployeeModel(),
     }
   },
   computed: {
@@ -137,43 +127,45 @@ export default {
   },
   watch: {
     employeeData() {
-      this.model = new EmployeeListFormModel(this.employeeData.email, this.employeeData.firstName, this.employeeData.lastName);
+      this.model = new EmployeeModel(this.employeeData.email, this.employeeData.firstName, this.employeeData.lastName);
     }
   },
   methods: {
     ...mapActions(['hideModal']),
+    getDepartments(positionModel) {
+      if (!positionModel.country_id) {
+        return [];
+      }
+
+      const country = this.dropdownData.countries.find(c => c.id === positionModel.country_id);
+
+      if (!country) {
+        return [];
+      }
+
+      return country.departments;
+    },
     onHideModal() {
-      this.model.email = null;
-      this.model.email = null;
+      this.model = new EmployeeModel();
       this.hideModal();
     },
-    addDepartment(dp, newDepartment) {
-      const tag = {
-        value: newDepartment,
-        text: newDepartment
-      };
-      dp.department.push(tag);
-    },
     addNewPosition: function () {
-      this.userDepartment.push({
-        department: []
-      })
+      this.model.positions.push(new PositionModel());
     },
-
+    removePosition: function(index) {
+      Vue.delete(this.model.positions, index)
+    },
     addNewRole: function () {
-      this.userRole.push(Vue.util.extend({}, this.userRole))
+      this.model.roles.push(new RoleModel());
     },
     removeRole: function (index) {
-      Vue.delete(this.userRole, index);
-    },
-    filterOptions: function (country) {
-      let opt = this.dropdownData.departments;
-      opt = opt.filter(i => i.country === country);
-      return opt;
+      Vue.delete(this.model.roles, index);
     },
     async onSubmit() {
-
+      console.log(this.model);
     }
+  },
+  mounted() {
   }
 }
 </script>
