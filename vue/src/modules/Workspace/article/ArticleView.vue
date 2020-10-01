a63b51ac466a4e99e1b691851fec3533f5261b29
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-10-01 17:54:18 +0400

    Working buttons

--- vue/src/modules/Workspace/article/ArticleView.vue
+++ vue/src/modules/Workspace/article/ArticleView.vue
@@ -3,6 +3,25 @@
     <div class="page-header">
       <back-button/>
       <b-breadcrumb :items="breadCrumb" class="d-none d-sm-flex"></b-breadcrumb>
+      <b-button variant="info">
+        <i class="fas fa-plus"></i>
+        {{ buttonTitle }}
+      </b-button>
+      <!--<b-dropdown variant="link" no-caret right>
+              <template v-slot:button-content>
+                <b-button size="sm" pill variant="link">
+                  <i class="fas fa-plus-circle fa-2x"></i>
+                </b-button>
+              </template>
+              <b-dropdown-item>
+                <i class="fas fa-plus"></i>
+                {{ $t('Create new folder') }}
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <i class="fas fa-plus"></i>
+                {{ $t('Create new article') }}
+              </b-dropdown-item>
+            </b-dropdown>-->
     </div>
   </div>
 </template>
@@ -17,10 +36,16 @@ export default {
   name: "ArticleView",
   components: {BackButton},
   computed: {
-    ...mapState(['breadCrumb'])
+    ...mapState(['breadCrumb', 'currentArticle']),
+    buttonTitle() {
+      if (this.currentArticle.depth === 0 && this.currentArticle.workspace.folder_in_folder) {
+        return this.$t('Create new folder')
+      }
+      return this.$t('Create new article')
+    },
   },
   methods: {
-    ...mapActions(['getArticleBreadCrumb']),
+    ...mapActions(['getArticleBreadCrumb', 'getCurrentArticle']),
     async getBreadCrumb() {
       const res = await this.getArticleBreadCrumb(this.$route.params.id)
       if (!res.success) {
@@ -30,6 +55,7 @@ export default {
   },
   mounted() {
     this.getBreadCrumb();
+    this.getCurrentArticle(this.$route.params.id);
   }
 }
 </script>
