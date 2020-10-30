b40d57876375da2b2e68502a8f36a98dd966f3a7
Saiat Kalbiev <kalbievich11@gmail.com>
2020-10-30 20:30:39 +0400

    Add password validation to change password model

--- modules/v1/users/models/ChangePassword.php
+++ modules/v1/users/models/ChangePassword.php
@@ -31,6 +31,12 @@ class ChangePassword extends Model
     {
         return [
             [['old_password', 'password', 'confirm_password'], 'required'],
+            [
+                'password',
+                'match',
+                'pattern' => '/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/',
+                'message' => \Yii::t('app', 'Should contain at least 1 lower case, 1 upper case, 1 digit and min length of 8')
+            ],
             ['old_password', 'validatePassword'],
             ['password', 'compare', 'compareAttribute' => 'confirm_password']
         ];
@@ -49,7 +55,7 @@ class ChangePassword extends Model
      * This method serves as the inline validation for password.
      *
      * @param string $attribute the attribute currently being validated
-     * @param array  $params    the additional name-value pairs given in the rule
+     * @param array $params the additional name-value pairs given in the rule
      */
     public function validatePassword(string $attribute, $params)
     {
