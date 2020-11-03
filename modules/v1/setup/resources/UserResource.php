ea8e5ec2fd1a0aed8e1fcf6231f523b7fe60c10f
Saiat Kalbiev <kalbievich11@gmail.com>
2020-11-03 19:12:37 +0400

    User getImageUrl in UserResource fields instead of direct logic

--- modules/v1/setup/resources/UserResource.php
+++ modules/v1/setup/resources/UserResource.php
@@ -23,7 +23,7 @@ class UserResource extends User
         return [
             'id', 'email', 'username', 'first_name', 'last_name', 'mobile', 'phone', 'birthday', 'about_me', 'hobbies',
             'image_url' => function () {
-                return $this->image_path ? env('API_HOST') . $this->image_path : null;
+                return $this->getImageUrl();
             },
             'display_name' => function () {
                 return $this->first_name . ' ' . $this->last_name;
