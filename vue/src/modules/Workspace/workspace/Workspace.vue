f0d52efa8776ad01ec8af39f6aa53321d340db17
Saiat Kalbiev <kalbievich11@gmail.com>
2020-11-09 17:05:38 +0400

    Remove workspace item component

--- vue/src/modules/Workspace/workspace/Workspace.vue
+++ vue/src/modules/Workspace/workspace/Workspace.vue
@@ -8,12 +8,26 @@
         {{ $t('Add New Workspace') }}
       </b-button>
     </div>
-
     <div class="page-content p-3">
       <div class="workspace-list">
         <div class="row">
-          <div v-for="(workspace, index) in workspaces" :key="`workspace-wrapper-${workspace.id}`" class="col-lg-2 col-sm-6 mb-3">
-            <WorkspaceItem :workspace="workspace" :index="index" />
+          <div v-for="(workspace) in workspaces" :key="`workspace-wrapper-${workspace.id}`"
+               class="col-lg-2 col-sm-6 mb-3">
+            <b-card no-body class="workspace-item overflow-hidden">
+              <div class="image-wrapper d-flex align-items-center justify-content-center">
+                <router-link :to="{name: 'workspace.view', params: {id: workspace.id}}">
+                  <b-card-img :src="workspace.image_url || '/assets/logo.png'" class="rounded-0"/>
+                </router-link>
+              </div>
+              <b-card-body>
+                <b-card-title title-tag="h5">
+                  <router-link :to="{name: 'workspace.view', params: {id: workspace.id}}">
+                    {{ workspace.name }}
+                  </router-link>
+                </b-card-title>
+              </b-card-body>
+              <dropdown-button :model="workspace" type="workspace"/>
+            </b-card>
           </div>
         </div>
       </div>
@@ -24,13 +38,13 @@
 <script>
 import {createNamespacedHelpers} from "vuex";
 import ContentSpinner from "../../../core/components/ContentSpinner";
-import WorkspaceItem from "./WorkspaceItem";
+import DropdownButton from "@/modules/Workspace/components/DropdownButton";
 
 const {mapState, mapActions} = createNamespacedHelpers('workspace');
 
 export default {
   name: "Workspace",
-  components: {WorkspaceItem, ContentSpinner},
+  components: {DropdownButton, ContentSpinner},
   data() {
     return {
       breadCrumb: [
@@ -55,6 +69,25 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.workspace-item {
+  height: 100%;
+  transition: all 0.3s;
 
+  &:hover {
+    box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.1);
+  }
+
+  .image-wrapper {
+    height: 140px;
+  }
+
+  .description {
+    & /deep/ p {
+      &:last-of-type {
+        margin-bottom: 0;
+      }
+    }
+  }
+}
 </style>
