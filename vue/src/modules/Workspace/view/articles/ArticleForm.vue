0555719bbe6ddc07e4e34b5c2167a5ef9fc7e515
Saiat Kalbiev <kalbievich11@gmail.com>
2020-11-09 19:01:37 +0400

    Implement article form, list and model in workspace module

---
+++ vue/src/modules/Workspace/view/articles/ArticleForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <ValidationObserver ref="form" v-slot="{ handleSubmit, invalid ,reset}">
+    <b-modal
+      :visible="show" id="article-form" ref="modal" size="lg" :title="modalTitle"
+      @hidden="hideModal" @ok.prevent="handleSubmit(onSubmit)" :ok-title="$t('Submit')" scrollable>
+      <b-form @submit.prevent="handleSubmit(onSubmit)" novalidate>
+        <input-widget :model="model" attribute="title"></input-widget>
+        <input-widget :model="model" attribute="body" type="richtext"></input-widget>
+      </b-form>
+    </b-modal>
+  </ValidationObserver>
+</template>
+
+<script>
+import InputWidget from "@/core/components/input-widget/InputWidget";
+import ArticleFormModel from "./ArticleFormModel";
+import {createNamespacedHelpers} from "vuex";
+
+const {mapState, mapActions} = createNamespacedHelpers('workspace');
+
+export default {
+  name: "ArticleForm",
+  components: {InputWidget},
+  data() {
+    return {
+      model: new ArticleFormModel(),
+    }
+  },
+  computed: {
+    ...mapState({
+      show: state => state.view.articles.modal.show,
+      modalObject: state => state.view.articles.modal.object,
+      currentWorkspace: state => state.currentWorkspace,
+    }),
+    modalTitle() {
+      if (this.model.id) {
+        return this.$t(`Update article '{name}'`, {name: this.model.title});
+      }
+      return this.$t(`Create new article`);
+    },
+  },
+  watch: {
+    modalObject() {
+      if (this.modalObject) {
+        this.model = new ArticleFormModel(this.modalObject);
+      }
+    },
+  },
+  methods: {
+    ...mapActions(['hideArticleModal', 'createArticle', 'updateArticle']),
+    async onSubmit() {
+      let response;
+
+      if (this.model.id) {
+        response = await this.updateArticle(this.model);
+      } else {
+        response = await this.createArticle(this.model);
+      }
+
+      if (response.success) {
+        const action = this.model.id ? 'updated' : 'created';
+        this.$toast(this.$t(`The article '{title}' was successfully ${action}`, {title: this.model.title}));
+        this.hideModal()
+      } else {
+        this.model.setMultipleErrors(response.body);
+      }
+    },
+    hideModal() {
+      this.hideArticleModal();
+      this.model = new ArticleFormModel()
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
--- vue/src/modules/Workspace/view/articles/Articles.vue
+++ vue/src/modules/Workspace/view/articles/Articles.vue
@@ -1,12 +1,69 @@
 <template>
-  <div>
-    articles
+  <div class="articles-wrapper">
+    <div class="row">
+      <div class="col-md-12 text-right pb-3">
+        <b-button @click="onCreateArticleClick" size="sm" variant="outline-primary">
+          <i class="fas fa-plus-circle"/>
+          {{ $t('Create Article') }}
+        </b-button>
+      </div>
+    </div>
+    <div class="article-item" v-for="(article) in articles" :key="`article-item-${article.id}`">
+      <b-card no-body class="mb-3">
+        <div class="p-3">
+          <router-link class="d-inline-block" :to="{name: 'article.view', params: {id: article.id}}">
+            <h5 class="mb-0">{{ article.title }}</h5>
+          </router-link>
+          <div v-if="article.short_description && article.short_description.length > 0"
+               v-html="article.short_description" class="mt-3"></div>
+          <small class="mt-3 text-muted d-block">
+            <i class="far fa-clock"></i>
+            {{ article.updated_at | relativeDate }}
+          </small>
+        </div>
+        <dropdown-buttons :item="article" @editClicked="onEditClicked" @removeClicked="onRemoveClicked"/>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
+
+import {createNamespacedHelpers} from "vuex";
+import DropdownButtons from "@/core/components/DropdownButtons";
+
+const {mapState, mapActions} = createNamespacedHelpers('workspace');
+
 export default {
-name: "Articles"
+  name: "Articles",
+  components: {DropdownButtons},
+  computed: {
+    ...mapState({
+      workspace: state => state.currentWorkspace,
+      articles: state => state.view.articles.data
+    })
+  },
+  methods: {
+    ...mapActions(['getArticles', 'showArticleModal', 'deleteArticle']),
+    onCreateArticleClick() {
+      this.showArticleModal({object: {workspace_id: this.workspace.id}});
+    },
+    onEditClicked(article) {
+      this.showArticleModal({object: article});
+    },
+    async onRemoveClicked(article) {
+      let success = await this.$confirm(this.$i18n.t("Are you sure you want to remove following article?"));
+      if (success) {
+        let response = await this.deleteArticle(article.id);
+        if (response.success) {
+          this.$toast(this.$i18n.t("Article was successfully deleted"));
+        }
+      }
+    },
+  },
+  beforeMount() {
+    this.getArticles(this.workspace.id);
+  }
 }
 </script>
 
