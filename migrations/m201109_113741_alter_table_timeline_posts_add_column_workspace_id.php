4f383179bbfef58922ee0c35e221290f6e16189e
Saiat Kalbiev <kalbievich11@gmail.com>
2020-11-09 16:05:36 +0400

    Create migration to add workspace_id foreign key to timeline posts table

---
+++ migrations/m201109_113741_alter_table_timeline_posts_add_column_workspace_id.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201109_113741_alter_table_timeline_posts_add_column_workspace_id
+ */
+class m201109_113741_alter_table_timeline_posts_add_column_workspace_id extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%timeline_posts}}', 'workspace_id', $this->integer(11)->after('id'));
+        $this->addForeignKey(
+            'fk_timeline_posts_workspace',
+            '{{%timeline_posts}}',
+            'workspace_id',
+            '{{%workspaces}}',
+            "id",
+            "CASCADE"
+        );
+        $this->createIndex(
+            '{{%idx-timeline_posts_workspace_id}}',
+            '{{%timeline_posts}}',
+            'workspace_id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('{{%idx-timeline_posts_workspace_id}}', '{{%timeline_posts}}');
+        $this->dropForeignKey('fk_timeline_posts_workspace', '{{%timeline_posts}}');
+        $this->dropColumn('{{%timeline_posts}}', 'workspace_id');
+    }
+
+}
