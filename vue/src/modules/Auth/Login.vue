7fadecfcaf0b68bb2d48f579c9f77691ccf5988c
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2020-11-10 12:13:32 +0400

    remove Register button

--- vue/src/modules/Auth/Login.vue
+++ vue/src/modules/Auth/Login.vue
@@ -5,7 +5,6 @@
         <img src="/assets/img/apollo11-white.png" alt="" style="width: 80px"/>
         <h3>{{ $t('Welcome') }}</h3>
         <p>You are 30 seconds away from entering in <b>Agora!</b></p>
-        <router-link class="btn btn-light btn-secondary btn-block" :to="{name: 'auth.register'}">Register</router-link>
       </div>
     </div>
     <div class="col-md-8 col-right">
