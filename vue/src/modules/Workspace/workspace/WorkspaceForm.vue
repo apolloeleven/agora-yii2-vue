6ba0a2ecb146c4e2dce125881cac8e8e58aa10e4
Zuka Gergaia <zuka.gergaia@gmail.com>
2020-11-17 13:45:06 +0400

    Enable workspace form submission using enter key

--- vue/src/modules/Workspace/workspace/WorkspaceForm.vue
+++ vue/src/modules/Workspace/workspace/WorkspaceForm.vue
@@ -10,8 +10,8 @@
           :model="model" attribute="image" type="file" :placeholder="$t('Choose a image or drop it here...')">
         </input-widget>
         <input-widget :model="model" attribute="folder_in_folder" type="checkbox"/>
-        <input-widget :model="model" attribute="name"/>
-        <input-widget :model="model" attribute="abbreviation"/>
+        <input-widget @keyup.enter="handleSubmit(onSubmit)" :model="model" attribute="name"/>
+        <input-widget @keyup.enter="handleSubmit(onSubmit)" :model="model" attribute="abbreviation"/>
         <input-widget :model="model" attribute="description" type="richtext"/>
       </b-form>
     </b-modal>
