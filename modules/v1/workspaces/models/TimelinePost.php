141eb86f46671bc21203683a50984dd24d728ee9
Zuka Gergaia <zuka.gergaia@gmail.com>
2020-11-19 15:03:18 +0400

    Updated TimeLinePost model to accommodate user_activity table

--- modules/v1/workspaces/models/TimelinePost.php
+++ modules/v1/workspaces/models/TimelinePost.php
@@ -4,6 +4,7 @@ namespace app\modules\v1\workspaces\models;
 
 use app\modules\v1\users\models\query\UserQuery;
 use app\modules\v1\users\models\User;
+use app\modules\v1\workspaces\behaviors\ActivityBehavior;
 use app\modules\v1\workspaces\models\query\TimelinePostQuery;
 use app\modules\v1\workspaces\models\query\WorkspaceQuery;
 use Yii;
@@ -61,6 +62,13 @@ class TimelinePost extends ActiveRecord
         return array_merge(parent::behaviors(), [
             TimestampBehavior::class,
             BlameableBehavior::class,
+            [
+                'class' => ActivityBehavior::class,
+                'workspaceIdAttribute' => 'workspace_id',
+                'tableNameAttribute' => TimelinePost::tableName(),
+                'contentIdAttribute' => 'id',
+                'descriptionAttribute' => 'description',
+            ]
         ]);
     }
 
