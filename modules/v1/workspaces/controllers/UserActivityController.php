3161333933f62a23669d322646ec3f63f79192a1
Zuka Gergaia <zuka.gergaia@gmail.com>
2020-11-19 19:19:19 +0400

    Updated UserActivity controller for querying db by workspace_id

--- modules/v1/workspaces/controllers/UserActivityController.php
+++ modules/v1/workspaces/controllers/UserActivityController.php
@@ -4,10 +4,30 @@
 namespace app\modules\v1\workspaces\controllers;
 
 use app\modules\v1\workspaces\models\UserActivity;
+use app\modules\v1\workspaces\resources\WorkspaceResource;
 use app\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+use yii\filters\auth\HttpBearerAuth;
+use yii\web\NotFoundHttpException;
 
 class UserActivityController extends ActiveController
 {
     public $modelClass = UserActivity::class;
 
+    public function actions()
+    {
+        $actions = parent::actions();
+        if(\Yii::$app->request->get('workspaceId')) {
+            $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+        }
+        return $actions;
+    }
+
+    public function prepareDataProvider() {
+        return new ActiveDataProvider([
+            'query' => $this->modelClass::find()->andWhere(['workspace_id' => \Yii::$app->request->get('workspaceId')])->orderBy([
+                'created_at' => SORT_DESC
+            ])
+        ]);
+    }
 }
