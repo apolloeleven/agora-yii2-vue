8f7b713b69ec11afc5fcc89c74c833d536c55ea6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-11-20 20:31:42 +0400

    User constants in ActivityBehavior

--- modules/v1/workspaces/behaviors/ActivityBehavior.php
+++ modules/v1/workspaces/behaviors/ActivityBehavior.php
@@ -6,14 +6,26 @@ namespace app\modules\v1\workspaces\behaviors;
 use app\modules\v1\workspaces\models\UserActivity;
 use yii\base\Behavior;
 use yii\db\ActiveRecord;
+use yii\helpers\Json;
 
 class ActivityBehavior extends Behavior
 {
-    public $workspaceIdAttribute;
-    public $tableNameAttribute;
-    public $contentIdAttribute;
-    public $actionAttribute;
-    public $descriptionAttribute;
+    /**
+     * @var \Closure
+     */
+    public $workspace_id = null;
+    /**
+     * @var string
+     */
+    public $contentIdAttribute = 'id';
+    /**
+     * @var string
+     */
+    public $description;
+    /**
+     * @var \Closure
+     */
+    public $data = null;
 
     public function events()
     {
@@ -27,26 +39,27 @@ class ActivityBehavior extends Behavior
     public function appendAction($action)
     {
         $userActivity = new UserActivity();
-        $userActivity->workspace_id = $this->owner->{$this->workspaceIdAttribute};
-        $userActivity->table_name = $this->tableNameAttribute;
+        $userActivity->workspace_id = $this->workspace_id instanceof \Closure ? call_user_func($this->workspace_id) : null;
+        $userActivity->table_name = $this->owner->tableName();
         $userActivity->content_id = $this->owner->{$this->contentIdAttribute};
         $userActivity->action = $action;
-        $userActivity->description = strip_tags($this->owner->{$this->descriptionAttribute});
+        $userActivity->description = $this->description;
+        $userActivity->data = $this->data ? Json::encode(call_user_func($this->data)) : null;
         $userActivity->save();
     }
 
     public function afterInsert()
     {
-        $this->appendAction('Insert');
+        $this->appendAction(UserActivity::ACTION_INSERT);
     }
 
     public function afterUpdate()
     {
-        $this->appendAction('Update');
+        $this->appendAction(UserActivity::ACTION_UPDATE);
     }
 
     public function beforeDelete()
     {
-        $this->appendAction('Delete');
+        $this->appendAction(UserActivity::ACTION_DELETE);
     }
 }
