a5f281b0a7676db9843130b6ba611b0c5fec4061
Zuka Gergaia <zuka.gergaia@gmail.com>
2020-11-20 14:46:04 +0400

    Added Activity view

---
+++ vue/src/modules/Workspace/view/activity/WorkspaceActivity.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    {{activities.length}}
+    <div v-for="(activity, index) in activities">
+      <div>
+        {{activity.action}}
+        {{index}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import {createNamespacedHelpers} from "vuex";
+
+const {mapState, mapActions} = createNamespacedHelpers('workspace');
+
+export default {
+  name: "WorkspaceActivity",
+  computed: {
+    ...mapState({
+      workspace: state => state.view.workspace,
+      activities: state => state.view.activity.data
+    })
+  },
+  methods: {
+    ...mapActions(['getActivities']),
+  },
+  beforeMount() {
+    this.getActivities(this.workspace.id);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
