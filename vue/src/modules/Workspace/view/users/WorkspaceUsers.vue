e15d560baf3fc8e6d8d070511352361033ea8030
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2020-11-24 11:40:44 +0400

    Add vuex to workspace-users

--- vue/src/modules/Workspace/view/users/WorkspaceUsers.vue
+++ vue/src/modules/Workspace/view/users/WorkspaceUsers.vue
@@ -3,126 +3,82 @@
     <content-spinner show/>
   </div>
   <div v-else class="workspace-users">
-    <b-card class="partial-shadow border-0 rounded-0">
-      <no-data-available v-if="users.length === 0" height="100"/>
-      <table class="table" v-else>
+    <b-card class="rounded-0 border-0" style="box-shadow: 2px 2px 4px lightgrey;">
+      <no-data-available v-if="!users" height="100"/>
+      <table class="table table-striped" v-else>
+        <thead class="title text-center">
         <tr>
-          <td class="title pl-5 border-0">
-            Workspace Users
-          </td>
-          <td class="title text-center border-0">
-            #Roles
-          </td>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Roles</th>
         </tr>
-        <tr v-for="(user,index) in users" v-bind:key="user.id">
-          <td :class="{'border-top-0': index === 0}" style="width: 60%">
-            <b-media class="pb-3 mt-3">
-              <template #aside>
-                <b-img width="44" class="user-image rounded-circle mx-5"
-                       :src="user.image_url ? user.image_url : '/assets/img/avatar.svg'"></b-img>
-              </template>
-              <p class="display-name mt-2">{{ user.displayName }}</p>
-            </b-media>
+        </thead>
+        <tbody class="text-center">
+        <tr class="py-0" v-for="user in users" :key="user.id">
+          <td class="align-middle py-1" style="width: 20%">
+            <b-img width="34" class="user-image rounded-circle mx-5"
+                   :src="user.image_url ? user.image_url : '/assets/img/avatar.svg'"></b-img>
           </td>
-          <td class="text-center align-middle role-font" :class="{'border-top-0': index === 0}">
-          <span v-for="role in user.roles" :class="roleClasses[role] ? roleClasses[role] : 'default-user'">
-              #{{ role }}
-          </span>
+          <td class="align-middle py-1 text-dark font-weight-bold">
+            {{ user['display_name'] }}
           </td>
+          <td class="align-middle py-2" style="width: 30%">
+            <span v-for="(role,index) in user.roles" :style="{'color': roleColor[role] || roleColor['default']}"
+                  class='px-1' :key="index">#{{ role }}</span></td>
         </tr>
+        </tbody>
       </table>
     </b-card>
   </div>
 </template>
 
 <script>
-import httpService from "@/core/services/httpService";
 import ContentSpinner from "@/core/components/ContentSpinner";
 import NoDataAvailable from "@/core/components/NoDataAvailable";
+import {createNamespacedHelpers} from "vuex";
+
+const {mapState, mapActions} = createNamespacedHelpers('workspace');
 
 export default {
   name: "WorkspaceUsers",
   components: {ContentSpinner, NoDataAvailable},
+  computed: {
+    ...mapState({
+      loading: state => state.view.users.loading,
+      users: state => state.view.users.data,
+    })
+  },
   data() {
     return {
-      users: null,
-      loading: true,
-      roleClasses: {
-        'admin': 'role-admin',
-        'workspaceAdmin': 'role-workspace-admin',
-        'user': 'role-user',
+      roleColor: {
+        'admin': '#246ba8',
+        'workspaceAdmin': '#dc7e6d',
+        'user': '#58a05e',
+        'default': '#8d8f92'
       },
     }
   },
+  methods: {
+    ...mapActions(['getWorkspaceUsers']),
+  },
   async beforeMount() {
     let workspaceId = parseInt(this.$route.params.id);
-    let {success, body} = await httpService.get('v1/workspaces/workspace/get-users?id=' + workspaceId)
-    if (success) {
-      this.users = body
-    }
-    this.loading = false;
+    await this.getWorkspaceUsers(workspaceId);
   },
 }
 </script>
 
 <style scoped>
 
-.pagination {
-  display: flex;
-  justify-content: center;
-}
-
 .workspace-users {
   width: 680px;
   margin: 0 auto;
 }
 
-.border-bottom {
-  border-bottom: 1px solid rgba(0, 0, 0, .05) !important;
-}
-
-.role-font {
-  font-weight: bolder;
-  font-size: 1rem;
-  text-shadow: 1px 0 2px #d7d7e5;
-}
-
-.role-admin {
-  color: #246ba8;
-}
-
-.role-workspace-admin {
-  color: #dc7e6d;
-}
-
-.role-user {
-  color: #58a05e;
-}
-
-.default-user {
-  color: #8d8f92;
-}
-
-.roles {
-  color: lightgrey;
-  font-size: 1rem;
-  text-shadow: 0 1px 2px lightgrey;
-}
-
-.partial-shadow {
-  box-shadow: 2px 2px 6px lightgrey !important;
-}
-
 .title {
-  font-size: 1.4rem;
+  font-size: 1rem;
   font-weight: bold;
   color: #bab5b5;
-}
-
-.display-name {
-  font-size: 1rem;
-  font-weight: 600;
-  color: #636060;
 }
 
 </style>
