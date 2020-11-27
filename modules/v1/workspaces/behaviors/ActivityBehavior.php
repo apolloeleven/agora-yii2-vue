1cf7c9fd5c0eefc19c139dae2ecec250ff976c97
Zuka Gergaia <zuka.gergaia@gmail.com>
2020-11-27 19:35:39 +0400

    Add configuration array for description and events to ActivityBehaviour

--- modules/v1/workspaces/behaviors/ActivityBehavior.php
+++ modules/v1/workspaces/behaviors/ActivityBehavior.php
@@ -3,9 +3,10 @@
 
 namespace app\modules\v1\workspaces\behaviors;
 
-use app\modules\v1\workspaces\models\UserActivity;
+use app\modules\v1\workspaces\models\WorkspaceActivity;
 use yii\base\Behavior;
 use yii\db\ActiveRecord;
+use yii\db\Exception;
 use yii\helpers\Json;
 
 class ActivityBehavior extends Behavior
@@ -23,11 +24,14 @@ class ActivityBehavior extends Behavior
     /**
      * @var string
      */
-    public $description = [
+    public $defaultTemplate = [
         'create' => '{user} created {model} {title}',
         'update' => '{user} updated {model} {title}',
-        'delete' => '{user} created {model} {title}',
+        'delete' => '{user} deleted {model} {title}',
     ];
+
+    public $template = [];
+
     /**
      * @var \Closure
      */
@@ -38,35 +42,43 @@ class ActivityBehavior extends Behavior
         return [
             ActiveRecord::EVENT_AFTER_INSERT => 'afterInsert',
             ActiveRecord::EVENT_AFTER_UPDATE => 'afterUpdate',
-            ActiveRecord::EVENT_AFTER_DELETE => 'afterDelete',
-            UserActivity::EVENT_SHARE => 'onShare'
+            ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
         ];
     }
 
     public function appendAction($action)
     {
-        $userActivity = new UserActivity();
+        $this->template = array_merge($this->defaultTemplate, $this->template);
+        $userActivity = new WorkspaceActivity();
         $userActivity->workspace_id = $this->workspace_id instanceof \Closure ? call_user_func($this->workspace_id) : null;
         $userActivity->table_name = $this->owner->tableName();
         $userActivity->content_id = $this->owner->{$this->contentIdAttribute};
         $userActivity->action = $action;
-        $userActivity->description = $this->description;
+        $userActivity->description = $this->template[$action];
         $userActivity->data = $this->data ? Json::encode(call_user_func($this->data)) : null;
-        $userActivity->save();
+        $saved = $userActivity->save();
+
+        if(!$saved) { //If arguments are invalid, save() fails, but no error is produced. Will reduce time of debugging. You are welcome :)
+            throw new Exception("Invalid Arguments");
+        }
     }
 
     public function afterInsert()
     {
-        $this->appendAction(UserActivity::ACTION_INSERT);
+        if(in_array(WorkspaceActivity::ACTION_INSERT, $this->events))
+            $this->appendAction(WorkspaceActivity::ACTION_INSERT);
+
     }
 
     public function afterUpdate()
     {
-        $this->appendAction(UserActivity::ACTION_UPDATE);
+        if(in_array(WorkspaceActivity::ACTION_UPDATE, $this->events))
+            $this->appendAction(WorkspaceActivity::ACTION_UPDATE);
     }
 
-    public function afterDelete()
+    public function beforeDelete()
     {
-        $this->appendAction(UserActivity::ACTION_DELETE);
+        if(in_array(WorkspaceActivity::ACTION_DELETE, $this->events))
+            $this->appendAction(WorkspaceActivity::ACTION_DELETE);
     }
 }
