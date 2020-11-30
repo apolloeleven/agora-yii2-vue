20ca3cf7c6ef42f2ff1a589ff971d78f8a0bfeee
Zuka Gergaia <zuka.gergaia@gmail.com>
2020-11-30 19:50:28 +0400

    Change formatting

--- modules/v1/workspaces/controllers/WorkspaceActivityController.php
+++ modules/v1/workspaces/controllers/WorkspaceActivityController.php
@@ -4,22 +4,24 @@
 namespace app\modules\v1\workspaces\controllers;
 
 use app\modules\v1\workspaces\models\WorkspaceActivity;
+use app\rest\ActiveController;
 use yii\data\ActiveDataProvider;
 
-class WorkspaceActivityController extends \yii\rest\ActiveController
+class WorkspaceActivityController extends ActiveController
 {
     public $modelClass = WorkspaceActivity::class;
 
     public function actions()
     {
         $actions = parent::actions();
-        if(\Yii::$app->request->get('workspaceId')) {
+        if (\Yii::$app->request->get('workspaceId')) {
             $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
         }
-        return $actions;
+        return $actions;?
     }
 
-    public function prepareDataProvider() {
+    public function prepareDataProvider()
+    {
         return new ActiveDataProvider([
             'query' => $this->modelClass::find()->andWhere(['workspace_id' => \Yii::$app->request->get('workspaceId')])->orderBy([
                 'created_at' => SORT_DESC
