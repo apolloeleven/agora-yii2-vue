58f9f011b9f2718c0eb49476bcab912a31c1c433
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-12-15 15:14:27 +0400

    Delete ActivityItemDescription.vue

--- vue/src/modules/Workspace/view/activity/ActivityItem.vue
+++ vue/src/modules/Workspace/view/activity/ActivityItem.vue
@@ -48,8 +48,6 @@
 </template>
 
 <script>
-import Vue from 'vue';
-import ActivityItemDescription from "@/modules/Workspace/view/activity/ActivityItemDescription";
 
 export default {
   name: "ActivityItem",
@@ -137,78 +135,7 @@ export default {
 
       return '/assets/img/avatar.svg';
     },
-
-    // prepareParent() {
-    //   if (this.activity.description.search('{parent}') === -1) {
-    //     return null;
-    //   }
-    //   let parentIdentity = JSON.parse(this.activity.parent_identity);
-    //   return parentIdentity.first_name + ' ' + parentIdentity.last_name;
-    // },
-    //
-    // prepareModel() {
-    //   if (this.activity.description.search('{model}') === -1) {
-    //     return null;
-    //   }
-    //
-    //   let model = this.activity.table_name.replaceAll(/\W/ig, "");
-    //   switch (model) {
-    //     case 'timeline_posts':
-    //       this.clickable = false; //temporary disable link until timeline post link is created
-    //       return 'post';
-    //     case 'articles':
-    //       this.path = "articles";
-    //       this.id = this.activity.content_id;
-    //       return 'article';
-    //     case 'folders':
-    //       this.path = "files";
-    //       this.id = this.activity.content_id;
-    //       return 'folder';
-    //   }
-    // },
-
-    // prepareTitle() {
-    //   if (this.activity.description.search('{title}') === -1) {
-    //     return;
-    //   }
-    //   this.activity.description = this.activity.description.replace("{title}", "");
-    //   let data = JSON.parse(this.activity.data);
-    //   let tableName = this.activity.table_name.replaceAll(/\W/ig, "");
-    //   let title = '';
-    //
-    //   switch (tableName) {
-    //     case 'timeline_posts':
-    //       title = data.description;
-    //       break;
-    //     case 'articles':
-    //       title = data.title;
-    //       break;
-    //     case 'folders':
-    //       title = data.name;
-    //       break;
-    //   }
-    //   if (this.activity.action === 'delete') {
-    //     this.path = '';
-    //     this.id = '';
-    //     this.clickable = false;
-    //   }
-    //   this.name = title.replace(/(<([^>]+)>)/gi, "");
-    // },
-
-    // prepareActivity() {
-    //   this.activity.description = this.activity.description.replace('{user}', this.activity.createdBy.username);
-    //
-    //   if (this.prepareParent())
-    //     this.activity.description = this.activity.description.replace('{parent}', this.prepareParent());
-    //   if (this.prepareModel())
-    //     this.activity.description = this.activity.description.replace('{model}', this.prepareModel());
-    //
-    //   this.prepareTitle();
-    // }
   },
-  beforeMount() {
-    // this.prepareActivity();
-  }
 }
 </script>
 
